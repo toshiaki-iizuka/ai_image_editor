e5b3f10aae534451aeb2bf9f6acb8228ef02f390
Toshiaki Iizuka <t_112ka@yahoo.co.jp>
2024-08-27 13:42:34 +0900

    feat: add to switch theme color

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,12 +1,18 @@
 "use client";
 
 import Image from "next/image";
-import UploadImage from "./upload/UploadImage";
 import LayerPanel from "./layer/LayerPanel";
+import UploadImage from "./upload/UploadImage";
+import { ModeToggle } from "./theme/ModeToggle";
 
 const Editor = () => {
 	return (
 		<div className="flex h-full">
+			<div className="py-6 px-4 min-w-48 ">
+				<div className="pb-12 text-center">
+					<ModeToggle />
+				</div>
+			</div>
 			<UploadImage />
 			<LayerPanel />
 		</div>
